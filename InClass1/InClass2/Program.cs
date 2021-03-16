02e6fd865cd42df4074023f37c4dcc1ebad2a548
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-03-16 18:14:56 -0500

    Finished dictionary

--- InClass1/InClass2/Program.cs
+++ InClass1/InClass2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace InClass2
 {
@@ -6,6 +7,30 @@ namespace InClass2
     {
         static void Main(string[] args)
         {
+            Dictionary<string, double> students = new Dictionary<string, double>();
+
+            students.Add("1", 3.0);
+            students.Add("2", 4.0);
+            students.Add("3", 2.7);
+
+            foreach (var studentID in students.Keys)
+            {
+                Console.Write($"\t {studentID}");
+            }
+
+            Console.WriteLine("\nWhose GPA do you want to see? >>?");
+            string idToLookFor = Console.ReadLine();
+
+            if (students.ContainsKey(idToLookFor) == true)
+            {
+                Console.WriteLine( $"{idToLookFor} has a {students[idToLookFor].ToString("N2")}"); 
+            }
+            else
+            {
+                Console.WriteLine($"{idToLookFor} is not a valid student.");
+                Environment.Exit(-1);
+            }
+
             
         }
     }
